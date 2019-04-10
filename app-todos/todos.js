09a458a5370ec319f8ff44ea32fd899db3905022
Thallis <thallis.s@hotmail.com>
2019-04-10 12:49:20 -0300

    App todos concluído

--- app-todos/todos.js
+++ app-todos/todos.js
@@ -1,21 +1,58 @@
-var listElement = document.querySelector('#app ul');
-var inputElement = document.querySelector('#app input');
-var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar JavaScript',
-    'Acessar comunidade RocketSeat'
-];
+var listElement = document.querySelector( '#app ul' );
+var inputElement = document.querySelector( '#app input' );
+var buttonElement = document.querySelector( '#app button' );
+var todos = JSON.parse(localStorage.getItem( 'list_todos' )) || [];
 
+
+// Renderiza todos
 function renderTodos(){
-    for (todo of todos) {
-        var todoElement = document.createElement('li');
-        var todoText = document.createTextNode(todo);
 
-        todoElement.appendChild( todoText )
-        listElement.appendChild( todoElement )
+    listElement.innerHTML = '';
+
+    for (todo of todos) {
+        var todoElement = document.createElement( 'li' );
+        var todoText = document.createTextNode( todo );
+        var excluir = document.createElement( 'a' );
+        var excluirText = document.createTextNode( 'Excluir' );
+        var pos = todos.indexOf( todo );
+
+        excluir.setAttribute( 'onclick', 'deleteTodo( '+ pos +' )' );
+        excluir.setAttribute( 'href', '#' );
+        excluir.appendChild( excluirText );
+
+        todoElement.appendChild( todoText );
+        todoElement.appendChild( excluir );
+
+        listElement.appendChild( todoElement );
     }
-};
+}
 
 renderTodos();
+
+
+// Adiciona todos
+function addTodo() {
+    var todoText = inputElement.value;
+    
+    todos.push( todoText );
+    inputElement.value = '';
+    renderTodos();
+    saveToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+
+// Deleta todos
+function deleteTodo( pos ) {
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+
+// Salva no local storage
+function saveToStorage() {
+    localStorage.setItem( 'list_todos', JSON.stringify( todos ) );
+}
